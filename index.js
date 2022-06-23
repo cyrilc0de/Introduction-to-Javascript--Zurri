3faa2559d721ee07718d645aa5b8aa7c2ab2cce6
Cyril <105249525+cyrilc0de@users.noreply.github.com>
2022-06-23 22:50:50 +0100

    Added the arithmetic calculation

---
+++ index.js
@@ -0,0 +1,16 @@
+var opr=prompt("what operation do you want to perform e.g +,-,*,/,")
+var num1 = parseint(prompt("input your first number:"))
+var num2 = parseint(prompt("input your second number:"))
+if(opr=="+")
+{
+   alert(num1 + num2)     
+}else if(opr=="-")
+{
+  alert(num1 - num2)      
+}else if (opr=="*")
+{
+  alert(num1*num2)      
+}else if(opr== "/")
+{
+  alert(num1 / num2)
+}
